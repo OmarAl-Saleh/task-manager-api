cc76c91970b2c360534b908f5dbbee5a9549f39b
OmarAl-Saleh <omaralsaleh1129@gmail.com>
2023-09-12 20:31:04 +0300

    edit mongodb version

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,12 +11,12 @@ const auth = async (req, res, next) => {
       _id: decoded._id,
       "tokens.token": token,
     }); // to find the user who is have the id the token we want to we use token for if is log out it will still valid to use
-    if (!user) throw new Error(); // if user not found to branch direct to catch
+    if (!user) throw new Error("inter findOne"); // if user not found to branch direct to catch
     req.token = token; // to give access to use token we just auth and send it to req in router in logout
     req.user = user; // to give access to use user we just auth and send it to req in router
     next();
   } catch (e) {
-    res.status(401).send({ error: "Please authenticate." });
+    res.status(401).send({ error: "Please authenticate.", message: e.message });
   }
 };
 module.exports = auth;
